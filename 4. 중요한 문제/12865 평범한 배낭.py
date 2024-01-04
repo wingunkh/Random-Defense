74249aaeb07343dcb2a4e255392eb5e3427cfc75
wingunkh <99gusrms@naver.com>
2024-01-04 13:40:39 +0900

    냅색 알고리즘

---
+++ 4. 중요한 문제/12865 평범한 배낭.py
@@ -0,0 +1,20 @@
+n, k = map(int, input().split())
+a = []
+dp = [[0 for _ in range(k+1)] for _ in range(n+1)]
+# dp[i][j] = 최대 무게가 j인 가방에서 i번째 물건까지 판단했을 때의 최대 가치
+
+for _ in range(n):
+    w, v = map(int, input().split())
+    a.append((w, v))
+
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        weight = a[i-1][0]
+        value = a[i-1][1]
+
+        if weight > j: # 가방에 물건을 넣을 수 없을 때
+            dp[i][j] = dp[i-1][j]
+        else: # 가방에 물건을 넣을 수 있을 때
+            dp[i][j] = max(dp[i-1][j-weight] + value, dp[i-1][j])
+
+print(dp[n][k])
