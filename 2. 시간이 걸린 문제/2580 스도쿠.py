37a99ec607a7d23a8ebc8973d1de7904fb062b09
wingunkh <99gusrms@naver.com>
2024-01-30 10:42:02 +0900

    normal

---
+++ 2. 시간이 걸린 문제/2580 스도쿠.py
@@ -0,0 +1,46 @@
+def dfs(depth):
+    if depth == len(blank):
+        for i in a:
+            print(*i, sep = ' ')
+
+        exit()
+
+    r, c = blank[depth]
+
+    for i in range(1, 10):
+        if check1(r, c, i) and check2(r, c, i):
+            a[r][c] = i
+            dfs(depth + 1)
+            a[r][c] = 0
+            
+def check1(r, c, n):
+    for i in range(9):
+        if a[r][i] == n:
+            return False
+
+    for i in range(9):
+        if a[i][c] == n:
+            return False
+
+    return True
+
+def check2(r, c, n):
+    r = r // 3 * 3
+    c = c // 3 * 3
+
+    for i in range(r, r + 3):
+        for j in range(c, c + 3):
+            if a[i][j] == n:
+                return False
+
+    return True
+    
+a = [list(map(int, input().split())) for _ in range(9)]
+blank = []
+
+for r in range(9):
+    for c in range(9):
+        if a[r][c] == 0:
+            blank.append((r, c))
+
+dfs(0)
