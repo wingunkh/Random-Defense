f336d84545e9099d642576b69229b79b747fd284
wingunkh <99gusrms@naver.com>
2024-01-31 21:16:06 +0900

    easy

--- 1. 쉽게 푼 문제/11659 구간 합 구하기 4.py
+++ 1. 쉽게 푼 문제/11659 구간 합 구하기 4.py
@@ -3,10 +3,12 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
-s = [0 for _ in range(n+1)]
+s = [0]
+tmp = 0
 
-for i in range(1, n+1):
-    s[i] = s[i-1] + a[i-1]
+for i in a:
+    tmp += i
+    s.append(tmp)
 
 for _ in range(m):
     start, end = map(int, input().split())
