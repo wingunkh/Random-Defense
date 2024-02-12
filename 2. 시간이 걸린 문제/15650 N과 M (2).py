3492d8b946f2557404696f42ce75f734fd6555e4
wingunkh <99gusrms@naver.com>
2024-02-12 13:49:14 +0900

    normal

--- 2. 시간이 걸린 문제/15650 N과 M (2).py
+++ 2. 시간이 걸린 문제/15650 N과 M (2).py
@@ -5,15 +5,11 @@ def dfs(x, depth):
         return
 
     for i in range(x, n+1):
-        if not visited[i]:
-            visited[i] = True
-            result.append(i)
-            dfs(i+1, depth+1)
-            visited[i] = False
-            result.pop()
+        result.append(i)
+        dfs(i+1, depth+1)
+        result.pop()
 
 n, m = map(int, input().split())
-visited = [False for _ in range(n+1)]
 result = []
 
 dfs(1, 0)
