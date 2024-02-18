28bc3a95f35926e3efc1ec7cb4e572fb054c207e
wingunkh <99gusrms@naver.com>
2024-02-18 11:59:14 +0900

    hard

---
+++ 3. 못 푼 문제/17298 오큰수.py
@@ -0,0 +1,14 @@
+n = int(input())
+a = list(map(int, input().split()))
+stack = []
+result = [-1 for _ in range(n)]
+
+stack.append(0)
+
+for i in range(1, n):
+    while stack and a[i] > a[stack[-1]]:
+        result[stack.pop()] = a[i]
+
+    stack.append(i)
+
+print(*result)
