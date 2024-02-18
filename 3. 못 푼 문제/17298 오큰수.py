14a32dd816362985354e17f192e1eca90ff95b27
wingunkh <99gusrms@naver.com>
2024-02-18 15:09:17 +0900

    hard

--- 3. 못 푼 문제/17298 오큰수.py
+++ 3. 못 푼 문제/17298 오큰수.py
@@ -6,7 +6,7 @@ result = [-1 for _ in range(n)]
 stack.append(0)
 
 for i in range(1, n):
-    while stack and a[i] > a[stack[-1]]:
+    while (stack and a[i] > a[stack[-1]]):
         result[stack.pop()] = a[i]
 
     stack.append(i)
