87d645ced573564fa9bcea85f049c93360270c5c
wingunkh <99gusrms@naver.com>
2024-02-18 11:59:19 +0900

    easy

--- 1. 쉽게 푼 문제/1931 회의실 배정.py
+++ 1. 쉽게 푼 문제/1931 회의실 배정.py
@@ -3,15 +3,14 @@ input = sys.stdin.readline
 
 n = int(input())
 a = [tuple(map(int, input().split())) for _ in range(n)]
-# 종료 시간 기준 오름차순 정렬
-# 종료 시간이 같을 때 사작 시간 기준 오름차순 정렬
-a.sort(key = lambda x: (x[1], x[0]))
 end = 0
 result = 0
 
+a.sort(key = lambda x: (x[1], x[0]))
+
 for i in a:
     if i[0] >= end:
-        result += 1
         end = i[1]
+        result += 1
 
 print(result)
