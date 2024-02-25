b2ca10707ead82a78ba5e0ea5409b8b9056e5bbc
wingunkh <99gusrms@naver.com>
2024-02-25 23:43:35 +0900

    hard

--- 3. 못 푼 문제/2493 탑.py
+++ 3. 못 푼 문제/2493 탑.py
@@ -6,7 +6,7 @@ result = [0 for _ in range(n)]
 for i in range(n):
     target = (a[i], i) # 다음 탑의 크기와 인덱스
     
-    while stack:
+    while stack: # 내부 루프의 총 반복 횟수가 N회이므로 이 코드의 시간복잡도는 O(N)이다.
         if stack[-1][0] > target[0]: # 다음 탑이 스택 top의 탑보다 작을 경우 
             result[target[1]] = stack[-1][1] + 1
             break
