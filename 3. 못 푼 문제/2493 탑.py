1edafcd51ccf5cd0d1c75540454d78843abea0c4
wingunkh <99gusrms@naver.com>
2024-02-25 10:18:51 +0900

    hard

---
+++ 3. 못 푼 문제/2493 탑.py
@@ -0,0 +1,21 @@
+n = int(input())
+a = list(map(int, input().split()))
+stack = []
+result = [0 for _ in range(n)]
+
+for i in range(n):
+    target = (a[i], i) # 다음 탑의 크기와 인덱스
+    
+    while stack:
+        if stack[-1][0] > target[0]: # 다음 탑이 스택 top의 탑보다 작을 경우 
+            result[target[1]] = stack[-1][1] + 1
+            break
+        else: # 다음 탑이 스택 top의 탑보다 클 경우
+            stack.pop()
+
+    if not stack:
+        result[target[1]] = 0
+
+    stack.append(target)
+
+print(*result, sep = ' ')
