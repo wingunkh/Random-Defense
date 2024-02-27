a4a957479ea3c5c798aff4f3a2619c67cbc96149
wingunkh <99gusrms@naver.com>
2024-02-27 15:46:54 +0900

    easy

--- 1. 쉽게 푼 문제/2579 계단 오르기.py
+++ 1. 쉽게 푼 문제/2579 계단 오르기.py
@@ -3,13 +3,13 @@ a = [0 for _ in range(301)]
 dp = [0 for _ in range(301)]
 # dp[i] = i번째 계단을 마지막으로 밟았을 때 점수의 최댓값
 
-for i in range(1, n+1):
+for i in range(1, n + 1):
     a[i] = int(input())
 
 dp[1] = a[1]
 dp[2] = a[1] + a[2]
 
-for i in range(1, n+1):
-    dp[i] = max(a[i] + dp[i-2], a[i] + a[i-1] + dp[i-3])
+for i in range(3, n + 1):
+    dp[i] = max(a[i] + a[i - 1] + dp[i - 3], a[i] + dp[i - 2])
 
 print(dp[n])
