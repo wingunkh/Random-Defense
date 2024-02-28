0d9c54a2420e8131a8a479b5b2b8ad01f6138667
wingunkh <99gusrms@naver.com>
2024-02-28 13:51:00 +0900

    easy

--- 1. 쉽게 푼 문제/2156 포도주 시식.py
+++ 1. 쉽게 푼 문제/2156 포도주 시식.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 a = [int(input()) for _ in range(n)]
 dp = [0 for _ in range(n)]
@@ -12,6 +15,6 @@ dp[1] = a[0] + a[1]
 dp[2] = max(a[0] + a[1], a[1] + a[2], a[0] + a[2])
 
 for i in range(3, n):
-    dp[i] = max(dp[i-1], dp[i-2] + a[i], dp[i-3] + a[i-1] + a[i])
+    dp[i] = max(a[i] + a[i - 1] + dp[i - 3], a[i] + dp[i - 2], dp[i - 1])
 
-print(max(dp))
+print(dp[-1])
