0601d2429e3c71dae78c7479c284c78cbad10e4a
wingunkh <99gusrms@naver.com>
2024-02-28 13:56:55 +0900

    hard

--- 3. 못 푼 문제/17298 오큰수.py
+++ 3. 못 푼 문제/17298 오큰수.py
@@ -5,7 +5,7 @@ result = [-1 for _ in range(n)]
 
 stack.append(0)
 
-for i in range(1, n): # 시간 복잡도 : O(2N)
+for i in range(1, n): # 내부 루프의 총 반복 횟수가 N회이므로 이 코드의 시간복잡도는 O(N)이다.
     while (stack and a[i] > a[stack[-1]]):
         result[stack.pop()] = a[i]
 
