1684ba153cddc38fe98789a75e133343e7bb9f58
wingunkh <99gusrms@naver.com>
2024-03-16 21:32:38 +0900

    easy

---
+++ 1. 쉽게 푼 문제/15651 N과 M (3).py
@@ -0,0 +1,15 @@
+def dfs(x, depth):
+    if depth == m:
+        print(*result, sep = ' ')
+
+        return
+
+    for i in range(1, n + 1):
+        result.append(i)
+        dfs(i + 1, depth + 1)
+        result.pop()
+
+n, m = map(int, input().split())
+result = []
+
+dfs(1, 0)
