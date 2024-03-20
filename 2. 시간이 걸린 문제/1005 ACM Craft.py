c022b48c37edc618ad0181225257e3d3bc4b2028
wingunkh <99gusrms@naver.com>
2024-03-20 15:18:27 +0900

    normal

---
+++ 2. 시간이 걸린 문제/1005 ACM Craft.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n, k = map(int, input().split())
+    a = [[] for _ in range(n + 1)]
+    time = [0] + list(map(int, input().split()))
+    indegree = [0 for _ in range(n + 1)]
+    q = deque()
+    dp = [0 for _ in range(n + 1)]
+    # dp[i] = i번 건물을 건설하는데 걸리는 시간
+
+    for _ in range(k):
+        s, e = map(int, input().split())
+        a[s].append(e)
+        indegree[e] += 1
+
+    for i in range(1, n + 1):
+        if indegree[i] == 0:
+            q.append(i)
+            dp[i] = time[i]
+
+    while q:
+        now = q.popleft()
+        
+        for i in a[now]:
+            indegree[i] -= 1
+            dp[i] = max(dp[i], dp[now] + time[i])
+
+            if indegree[i] == 0:
+                q.append(i)
+
+    target = int(input())
+
+    print(dp[target])
