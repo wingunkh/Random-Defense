2c61f06cb3332d121542b4b97a0f0b98240e5bc4
wingunkh <99gusrms@naver.com>
2024-04-20 13:10:46 +0900

    normal

--- 2. 시간이 걸린 문제/14500 테트로미노.py
+++ 2. 시간이 걸린 문제/14500 테트로미노.py
@@ -1,4 +1,7 @@
-def dfs(r, c, depth, sum):
+import sys
+input = sys.stdin.readline
+
+def dfs(r, c, sum, depth):
     global result
 
     if depth == 4:
@@ -14,36 +17,39 @@ def dfs(r, c, depth, sum):
         
         if not visited[next_r][next_c]:
             visited[next_r][next_c] = True
-            dfs(next_r, next_c, depth + 1, sum + a[next_r][next_c])
+            dfs(next_r, next_c, sum + a[next_r][next_c], depth + 1)
             visited[next_r][next_c] = False
 
 def func(r, c):
     global result
-
+    
     for i in range(4):
-        tmp = a[r][c]
+        sum = a[r][c]
         
         for j in range(3):
-            next_r, next_c = r + dr[(i + j) % 4], c + dc[(i + j) % 4]
-            
+            next_r, next_c = r + dr[(j + i) % 4], c + dc[(j + i) % 4]
+
             if not (0 <= next_r < n and 0 <= next_c < m):
                 continue
 
-            tmp += a[next_r][next_c]
+            sum += a[next_r][next_c]
 
-        result = max(result, tmp)
-
+        result = max(result, sum)
+    
 n, m = map(int, input().split())
-a = [list(map(int, input().split())) for _ in range(n)]
+a = []
 visited = [[False for _ in range(m)] for _ in range(n)]
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 result = 0
 
+for _ in range(n):
+    a.append(list(map(int, input().split())))
+
 for r in range(n):
     for c in range(m):
         visited[r][c] = True
-        dfs(r, c, 1, a[r][c])
+        dfs(r, c, a[r][c], 1)
         visited[r][c] = False
 
         func(r, c)
