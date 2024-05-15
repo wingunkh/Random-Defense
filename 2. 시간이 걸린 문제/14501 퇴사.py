84f55891a9a1307354cfc600dd635971caf078e7
wingunkh <99gusrms@naver.com>
2024-05-15 11:56:09 +0900

    normal

--- 2. 시간이 걸린 문제/14501 퇴사.py
+++ 2. 시간이 걸린 문제/14501 퇴사.py
@@ -1,6 +1,6 @@
 n = int(input())
 a = []
-dp = [0 for _ in range(n+1)]
+dp = [0 for _ in range(n + 1)]
 # dp[i] = i 일차까지 일했을 때 얻을 수 있는 최대 수익
 
 for _ in range(n):
@@ -8,7 +8,7 @@ for _ in range(n):
     a.append((time, money))
 
 for i in range(n):
-    for j in range(i + a[i][0], n+1):
+    for j in range(i + a[i][0], n + 1):
         dp[j] = max(dp[j], dp[i] + a[i][1])
 
 print(dp[-1])
