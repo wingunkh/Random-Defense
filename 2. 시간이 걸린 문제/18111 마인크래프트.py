f1b56fd06113883958b621e448e4ba08d509e6df
wingunkh <99gusrms@naver.com>
2024-05-18 13:50:23 +0900

    normal

--- 2. 시간이 걸린 문제/18111 마인크래프트.py
+++ 2. 시간이 걸린 문제/18111 마인크래프트.py
@@ -3,27 +3,30 @@ input = sys.stdin.readline
 
 n, m, b = map(int, input().split())
 a = [list(map(int, input().split())) for _ in range(n)]
-height = [0 for _ in range(257)]
-result_time = sys.maxsize
-result_height = 0
+height = [0 for _ in range(257)]  # height[i] = 높이가 i인 블록의 수
+result_time, result_height = sys.maxsize, 0
 
 for r in range(n):
     for c in range(m):
         height[a[r][c]] += 1
 
-for i in range(257):
-    create_count = 0
-    delete_count = 0
+for h in range(257):
+    need = 0
+    time = 0
+    
+    for i in range(257):
+        if i < h:
+            need += (h - i) * height[i]
+            time += (h - i) * height[i]
+        elif i > h:
+            need -= (i - h) * height[i]
+            time += 2 * (i - h) * height[i]
+    
+    if need > b:
+        continue
 
-    for j in range(257):
-        if j < i:
-            create_count += (i - j) * height[j]
-        elif j > i:
-            delete_count += (j - i) * height[j]
-
-    if create_count - delete_count <= b:
-        if create_count + delete_count * 2 <= result_time:
-            result_time = create_count + delete_count * 2
-            result_height = i
+    if time <= result_time:
+        result_time = time
+        result_height = h
 
 print(result_time, result_height)
