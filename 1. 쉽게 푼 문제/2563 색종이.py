ad7fec3d0aae72b5a57d43346e7e580e79861071
wingunkh <99gusrms@naver.com>
2024-05-20 13:32:43 +0900

    easy

--- 1. 쉽게 푼 문제/2563 색종이.py
+++ 1. 쉽게 푼 문제/2563 색종이.py
@@ -1,18 +1,17 @@
-a = [[0 for _ in range(101)] for _ in range(101)]
 n = int(input())
+a = [[0 for _ in range(101)] for _ in range(101)]
 result = 0
 
 for _ in range(n):
-    x, y = map(int, input().split())
-    start_r, start_c = y, x
-    
-    for r in range(start_r, start_r + 10):
-        for c in range(start_c, start_c + 10):
+    y, x = map(int, input().split())
+
+    for r in range(y, y + 10):
+        for c in range(x, x + 10):
             a[r][c] = 1
 
 for r in range(101):
     for c in range(101):
-        if a[r][c] == 1:
+        if a[r][c]:
             result += 1
 
 print(result)
