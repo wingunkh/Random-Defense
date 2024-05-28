9fdebeba269df7bd2c8c78b469ca8ad1849540cd
wingunkh <99gusrms@naver.com>
2024-05-28 13:26:00 +0900

    normal

--- 2. 시간이 걸린 문제/2504 괄호의 값.py
+++ 2. 시간이 걸린 문제/2504 괄호의 값.py
@@ -4,33 +4,36 @@ tmp = 1
 result = 0
 
 for i in range(len(s)):
-    k = 1
     if s[i] == '(':
         stack.append(s[i])
         tmp *= 2
-    elif s[i] == '[':     
+    elif s[i] == '[':
         stack.append(s[i])
         tmp *= 3
     elif s[i] == ')':
         if not stack or stack[-1] == '[':
             result = 0
             break
-        elif s[i - 1] == '(':
+        
+        stack.pop()
+        
+        if s[i - 1] == '(':
             result += tmp
             
-        stack.pop()
         tmp //= 2
     else:
         if not stack or stack[-1] == '(':
             result = 0
             break
-        elif s[i - 1] == '[':
+        
+        stack.pop()
+        
+        if s[i - 1] == '[':
             result += tmp
             
-        stack.pop()
         tmp //= 3
-        
-if stack:
-    print(0)
 else:
-    print(result)
+    if stack:
+        result = 0
+
+print(result)
