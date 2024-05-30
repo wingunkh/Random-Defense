60d62782802df43346a2037183643839cb1d11d2
wingunkh <99gusrms@naver.com>
2024-05-30 14:07:17 +0900

    easy

--- 1. 쉽게 푼 문제/25206 너의 평점은.py
+++ 1. 쉽게 푼 문제/25206 너의 평점은.py
@@ -1,33 +1,18 @@
-def func(grade):
-    if grade == "A+":
-        return 4.5
-    elif grade == "A0":
-        return 4.0
-    elif grade == "B+":
-        return 3.5
-    elif grade == "B0":
-        return 3.0
-    elif grade == "C+":
-        return 2.5
-    elif grade == "C0":
-        return 2.0
-    elif grade == "D+":
-        return 1.5
-    elif grade == "D0":
-        return 1.0
-    else:
-        return 0.0
-
+dic = {
+    'A+': 4.5, 'A0': 4.0,
+    'B+': 3.5, 'B0': 3.0,
+    'C+': 2.5, 'C0': 2.0,
+    'D+': 1.5, 'D0': 1.0,
+    'F': 0.0
+}
 sum = 0
 result = 0
 
-for i in range(20):
+for _ in range(20):
     buff = input().split()
-    
-    if buff[2] == 'P':
-        continue
-    
-    sum += float(buff[1])
-    result += float(buff[1]) * func(buff[2])
+
+    if buff[2] != 'P':
+        sum += float(buff[1])
+        result += float(buff[1]) * dic[buff[2]]
 
 print(result / sum)
