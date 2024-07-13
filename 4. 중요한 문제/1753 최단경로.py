785dc103f6b96148e9096d3f42670ff8737f43ed
wingunkh <99gusrms@naver.com>
2024-07-13 14:39:12 +0900

    다익스트라

--- 4. 중요한 문제/1753 최단경로.py
+++ 4. 중요한 문제/1753 최단경로.py
@@ -7,21 +7,24 @@ input = sys.stdin.readline
 V, E = map(int, input().split())
 start = int(input())
 a = [[] for _ in range(V + 1)] # 그래프를 인접 리스트로 구현
-distance = [sys.maxsize for _ in range(V + 1)] # 출발 노드와 이외의 모든 노드 간의 최단 거리 리스트
+distance = [sys.maxsize for _ in range(V + 1)] # 출발 노드와 이외의 모든 노드 간의 최단 경로 리스트
 pq = [] # 다익스트라 알고리즘은 현재까지 알고 있던 최단 경로를 토대로 최단 경로를 갱신
 
 for _ in range(E):
     u, v, w = map(int, input().split())
     a[u].append((v, w))
 
-distance[start] = 0 # 출발 노드에서 출발 노드까지의 최단 거리는 0
-heapq.heappush(pq, (0, start))
+distance[start] = 0 # 출발 노드에서 출발 노드까지의 최단 경로는 0
+heapq.heappush(pq, (0, start)) # (가중치, 목적지)
 
 while pq:
-    _, now = heapq.heappop(pq)
+    weight, now = heapq.heappop(pq)
+
+    if distance[now] < weight: # 더 이상 최단 경로가 아닌 경로일 경우 무시
+        continue
 
     for next, weight in a[now]:
-        if distance[next] > distance[now] + weight: # 최단 거리 갱신
+        if distance[next] > distance[now] + weight: # 최단 경로 갱신
             distance[next] = distance[now] + weight
             heapq.heappush(pq, (distance[next], next))
 
