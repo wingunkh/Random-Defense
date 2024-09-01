609749862872d8431a81c5e887927284433f3f86
wingunkh <99gusrms@naver.com>
2024-09-01 15:06:33 +0900

    easy

--- 1. 쉽게 푼 문제/9465 스티커.py
+++ 1. 쉽게 푼 문제/9465 스티커.py
@@ -4,19 +4,19 @@ for _ in range(t):
     n = int(input())
     a = [list(map(int, input().split())) for _ in range(2)]
     dp = [[0 for _ in range(n)] for _ in range(2)]
-    # dp[i][j] = j열에서 i행 스티커를 마지막으로 선택했을 때의 최대 점수
+    # dp[i][j] = i행 j열 스티커를 마지막으로 떼어냈을 때의 최대 점수
     dp[0][0] = a[0][0]
     dp[1][0] = a[1][0]
 
     if n == 1:
         print(max(dp[0][0], dp[1][0]))
         continue
-    
+
     dp[0][1] = dp[1][0] + a[0][1]
     dp[1][1] = dp[0][0] + a[1][1]
-    
-    for i in range(2, n):
-        dp[0][i] = a[0][i] + max(dp[1][i - 1], dp[1][i - 2])
-        dp[1][i] = a[1][i] + max(dp[0][i - 1], dp[0][i - 2])
+
+    for c in range(2, n):
+        dp[0][c] = a[0][c] + max(dp[1][c - 1], dp[1][c - 2])
+        dp[1][c] = a[1][c] + max(dp[0][c - 1], dp[0][c - 2])
 
     print(max(dp[0][-1], dp[1][-1]))
