ac1b92c11417ab09131ad36e89ebb1c92e9c548c
wingunkh <99gusrms@naver.com>
2024-10-03 14:38:50 +0900

    normal

--- 2. 시간이 걸린 문제/2143 두 배열의 합.py
+++ 2. 시간이 걸린 문제/2143 두 배열의 합.py
@@ -1,31 +1,38 @@
 t = int(input())
 n = int(input())
-a = [0] + list(map(int, input().split()))
+a = list(map(int, input().split()))
 m = int(input())
-b = [0] + list(map(int, input().split()))
-dic = {}
+b = list(map(int, input().split()))
+sub_a = {}
+sub_b = {}
 result = 0
 
-for i in range(1, n + 1):
-    a[i] = a[i] + a[i - 1]
+for i in range(n):
+    tmp = 0
 
-for i in range(1, m + 1):
-    b[i] = b[i] + b[i - 1]
+    for j in range(i, n):
+        tmp += a[j]
 
-for a_right in range(1, n + 1):
-    for a_left in range(a_right):
-        a_sum = a[a_right] - a[a_left]
-
-        if a_sum not in dic:
-            dic[a_sum] = 1
+        if tmp not in sub_a:
+            sub_a[tmp] = 1
         else:
-            dic[a_sum] += 1
+            sub_a[tmp] += 1
 
-for b_right in range(1, m + 1):
-    for b_left in range(b_right):
-        b_sum = b[b_right] - b[b_left]
+for i in range(m):
+    tmp = 0
 
-        if t - b_sum in dic:
-            result += dic[t - b_sum]
+    for j in range(i, m):
+        tmp += b[j]
+        
+        if tmp not in sub_b:
+            sub_b[tmp] = 1
+        else:
+            sub_b[tmp] += 1
+
+for i in sub_a:
+    target = t - i
+
+    if target in sub_b:
+        result += sub_a[i] * sub_b[target]
 
 print(result)
