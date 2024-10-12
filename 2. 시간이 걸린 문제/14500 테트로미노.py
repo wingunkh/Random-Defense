e6ac8311c0688c9eabfcb308167241a7df595793
wingunkh <99gusrms@naver.com>
2024-10-12 16:27:53 +0900

    normal

--- 2. 시간이 걸린 문제/14500 테트로미노.py
+++ 2. 시간이 걸린 문제/14500 테트로미노.py
@@ -20,31 +20,28 @@ def dfs(r, c, sum, depth):
             dfs(next_r, next_c, sum + a[next_r][next_c], depth + 1)
             visited[next_r][next_c] = False
 
-def func(r, c):
+def tshape(r, c):
     global result
     
     for i in range(4):
         sum = a[r][c]
-        
+
         for j in range(3):
-            next_r, next_c = r + dr[(j + i) % 4], c + dc[(j + i) % 4]
+            next_r, next_c = r + dr[(i + j) % 4], c + dc[(i + j) % 4]
 
             if not (0 <= next_r < n and 0 <= next_c < m):
-                continue
+                break
 
             sum += a[next_r][next_c]
+        else:
+            result = max(result, sum)
 
-        result = max(result, sum)
-    
 n, m = map(int, input().split())
-a = []
+a = [list(map(int, input().split())) for _ in range(n)]
 visited = [[False for _ in range(m)] for _ in range(n)]
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 result = 0
-
-for _ in range(n):
-    a.append(list(map(int, input().split())))
 
 for r in range(n):
     for c in range(m):
@@ -52,6 +49,6 @@ for r in range(n):
         dfs(r, c, a[r][c], 1)
         visited[r][c] = False
 
-        func(r, c)
-
+        tshape(r, c)
+        
 print(result)
